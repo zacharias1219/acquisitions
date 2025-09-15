c1a9920353b20d44f4b0a4bc56b65b5c7481fd25
Richard Abishai <rickoshade1891@gmail.com>
2025-09-16 00:01:39 +0530

    Refactor project structure: move server logic to src directory, update scripts, and remove old index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
