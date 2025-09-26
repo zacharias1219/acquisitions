6dbf45c0307f5b009873a2a818e6871498b6d3a0
Richard Abishai <rickoshade1891@gmail.com>
2025-09-26 14:57:35 +0530

    Implement middleware and logging enhancements: add security, logging, and cookie parsing; update root and health check responses

--- src/app.js
+++ src/app.js
@@ -1,9 +1,54 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
+import usersRoutes from '#routes/users.routes.js';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
+app.use(securityMiddleware);
+
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello World' });
+  logger.info('Hello from Acquisitions!');
+
+  res.status(200).send('Hello from Acquisitions!');
+});
+
+app.get('/health', (req, res) => {
+  res
+    .status(200)
+    .json({
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime(),
+    });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
 });
 
 export default app;
